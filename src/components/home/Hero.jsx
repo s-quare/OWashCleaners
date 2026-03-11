a5d9686661684bb147d490dc35f4e5140bc74ef4
s-quare <okeyprosperebubealex14@gmail.com>
2026-03-11 01:18:19 +0100

    loader

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -25,16 +25,11 @@ export default function Hero() {
         <div className="fixed grid place-items-center z-1000 h-screen w-screen bg-white">
           <div>
             <div className="h-10 w-10 mx-auto mb-5 border-5 border-brand-gold/30 border-t-brand-gold rounded-full animate-spin" />
-            <p className="text-center text-xl tracking-wider font-bold text-gray-700">
-              OWashCleaners
-            </p>
           </div>
         </div>
       )}
-      {/* Background/Image Layer */}
-      {/* Absolute & Full on mobile, becomes side-by-side on lg */}
+
       <div className="absolute inset-0 lg:relative lg:w-1/2 lg:h-full z-0">
-        {/* Overlay only on mobile/md to help text readability */}
         <div className="absolute inset-0 bg-black/50 lg:bg-transparent z-10"></div>
         <div className="absolute h-full w-[60%] bg-linear-to-r from-black/40 to-black/0 lg:bg-transparent z-10"></div>
         <img
@@ -46,7 +41,6 @@ export default function Hero() {
       </div>
 
       {/* Text Layer */}
-      {/* Centered on mobile, standard layout on lg */}
       <div className="relative z-20 w-full px-6 md:px-12 lg:w-1/2 lg:pl-16 flex flex-col justify-center">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
@@ -55,8 +49,8 @@ export default function Hero() {
           className="text-white lg:text-off-black"
         >
           <p className="text-[10px] mb-5 bg-white/50 text-off-black/80 w-fit px-2 py-px rounded-full ">
-            <i className="bi bi-leaf text-green-300"></i> 100% Eco-friendly Cleaning
-            solutions
+            <i className="bi bi-leaf text-green-300"></i> 100% Eco-friendly
+            Cleaning solutions
           </p>
           <h1 className="text-5xl lg:text-7xl font-bold leading-tight mb-6">
             The New Standard <br /> Of{" "}
